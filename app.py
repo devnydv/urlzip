d7b3562c2c296251814eaecef241a8ab4ca7551d
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-22 21:29:04 +0530

    Update app.py
    Added user route

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def signup():
 def login():
     return render_template("login.html")
 
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
 if __name__=="__main__":
     app.run(debug=True)
 
