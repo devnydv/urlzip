699153e04524932d383f8e243d7a40b36e75279f
devnydv <bittumailb2@gmail.com>
2024-08-04 11:28:57 +0530

    bug fix in delete

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from urljar import app
-#from test import test
-
-# test file is ignores by git need correction before push
-#app.register_blueprint(test)
 
 if __name__=="__main__":
     app.run(debug=True)
