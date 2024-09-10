a66eb73a846e4e332ffc1b759a39e93bf305d9df
devnydv <bittumailb2@gmail.com>
2024-09-10 12:56:38 +0530

    comit

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from urljar import app
-<<<<<<< HEAD
 from htmx import htmx
 
 
 # test file is ignores by git need correction before push
 
 app.register_blueprint(htmx)
-
-=======
->>>>>>> dbce618ebaa9798233c9f5c2fee3c43e08440402
 
 if __name__=="__main__":
     app.run(debug=True)
